9d564bd66802e24081ca7db948eb6d4b27125a56
bunnie <bunnie@kosagi.com>
2024-10-28 14:34:46 +0800

    refactoring around the image api as I learn it better

--- homography/src/main.rs
+++ homography/src/main.rs
@@ -1,21 +1,26 @@
 mod homography;
+use std::any::Any;
+
 use homography::*;
 mod qr;
 use qr::*;
 mod minigfx;
 use minigfx::*;
 mod glue;
-use image::Pixel;
+use image::{DynamicImage, GenericImageView, GrayImage, Rgb, RgbImage, RgbaImage};
+use image::{ImageBuffer, Pixel};
+use minifb::{Key, Window, WindowOptions};
 
-use image::{DynamicImage, GenericImageView, Rgb, RgbImage};
-use minifb::{Window, WindowOptions, Key};
-
-fn show_image(img: &DynamicImage) {
+fn show_image(flipped_img: &DynamicImage) {
     // Get image dimensions and raw pixel data
-    let (width, height) = img.dimensions();
+    let (width, height) = flipped_img.dimensions();
 
-    let img_data = img.to_rgba8();
-    let buffer: Vec<u32> = img_data
+    // Flip the coordinate system
+    let mut img = RgbaImage::new(flipped_img.dimensions().0, flipped_img.dimensions().1);
+    for (x, y, pixel) in flipped_img.clone().into_rgba8().enumerate_pixels() {
+        img.put_pixel(x, flipped_img.dimensions().1 - y - 1, *pixel);
+    }
+    let buffer: Vec<u32> = img
         .chunks(4)
         .map(|pixel| {
             let [r, g, b, _a] = [pixel[0], pixel[1], pixel[2], pixel[3]];
@@ -24,34 +29,33 @@ fn show_image(img: &DynamicImage) {
         .collect();
 
     // Create a window
-    let mut window = Window::new(
-        "PNG Viewer",
-        width as usize,
-        height as usize,
-        WindowOptions::default(),
-    )
-    .expect("Failed to create window");
+    let mut window = Window::new("PNG Viewer", width as usize, height as usize, WindowOptions::default())
+        .expect("Failed to create window");
 
     // Display the image
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        window
-            .update_with_buffer(&buffer, width as usize, height as usize)
-            .expect("Failed to update buffer");
+        window.update_with_buffer(&buffer, width as usize, height as usize).expect("Failed to update buffer");
     }
 }
 
 fn main() {
     // Load the PNG file
-    let img = image::open("../images/test256b.png").expect("Failed to load image");
+    let img = image::open("../images/test256b.png").expect("Failed to load image").into_luma8();
+
+    // Handle to the luma version for image processing
+    let image = &img;
+    // Create an RGB version for UI debugging
+    let mut rgb_image = RgbImage::new(img.dimensions().0, img.dimensions().1);
+    for (x, y, pixel) in image.enumerate_pixels() {
+        let luma = pixel[0];
+        rgb_image.put_pixel(x, y, Rgb([luma, luma, luma]));
+    }
+    let mut drawable_image = &mut rgb_image;
 
     // Get image dimensions and raw pixel data
     let (width, _height) = img.dimensions();
-    let mut candidates: [Option::<Point>; 8] = [None; 8];
-    let image = img.clone().into_luma8();
-    let hom_src = img.clone().into_luma8();
+    let mut candidates: [Option<Point>; 8] = [None; 8];
     find_finders(&mut candidates, &image, 128, width as _);
-
-    let mut drawable_image = img.to_rgb8();
 
     const CROSSHAIR_LEN: isize = 3;
     let mut candidates_found = 0;
@@ -87,7 +91,7 @@ fn main() {
                 Line::new_with_style(
                     c_screen + Point::new(0, CROSSHAIR_LEN),
                     c_screen - Point::new(0, CROSSHAIR_LEN),
-                    DrawStyle::stroke_color(Rgb([0,255,0]).into())
+                    DrawStyle::stroke_color(Rgb([0, 255, 0]).into()),
                 ),
                 None,
                 false,
@@ -98,53 +102,81 @@ fn main() {
                 Line::new_with_style(
                     c_screen + Point::new(CROSSHAIR_LEN, 0),
                     c_screen - Point::new(CROSSHAIR_LEN, 0),
-                    DrawStyle::stroke_color(Rgb([0,255,0]).into())
+                    DrawStyle::stroke_color(Rgb([0, 255, 0]).into()),
                 ),
                 None,
                 false,
             );
         }
-        show_image(&DynamicImage::ImageRgb8(drawable_image));
+        show_image(&DynamicImage::ImageRgb8(drawable_image.clone()));
 
-        let mut dest_img = RgbImage::new(hom_src.dimensions().0, hom_src.dimensions().1);
+        let mut dest_img = RgbImage::new(image.dimensions().0, image.dimensions().1);
         match find_homography(src, dst) {
             Some(h) => {
                 println!("{:}", h);
                 // iterate through pixels and apply homography
-                for y in 0..hom_src.dimensions().1 {
-                    for x in 0..hom_src.dimensions().0 {
+                for y in 0..image.dimensions().1 {
+                    for x in 0..image.dimensions().0 {
                         let (x_dst, y_dst) = apply_homography(&h, (x as f32, y as f32));
-                        if (x_dst as i32 >= 0) && ((x_dst as i32) < hom_src.dimensions().0 as i32)
-                        && (y_dst as i32 >= 0) && ((y_dst as i32) < hom_src.dimensions().1 as i32) {
+                        if (x_dst as i32 >= 0)
+                            && ((x_dst as i32) < image.dimensions().0 as i32)
+                            && (y_dst as i32 >= 0)
+                            && ((y_dst as i32) < image.dimensions().1 as i32)
+                        {
                             // println!("{},{} -> {},{}", x_src as i32, y_src as i32, x, y);
-                            dest_img.put_pixel(x_dst as u32, y_dst as u32, hom_src.get_pixel(x as u32, y as u32).to_rgb());
+                            dest_img.put_pixel(
+                                x_dst as u32,
+                                y_dst as u32,
+                                image.get_pixel(x as u32, y as u32).to_rgb(),
+                            );
                         }
 
                         let (x_dst, y_dst) = apply_homography(&h, (x as f32 + 0.5, y as f32));
-                        if (x_dst as i32 >= 0) && ((x_dst as i32) < hom_src.dimensions().0 as i32)
-                        && (y_dst as i32 >= 0) && ((y_dst as i32) < hom_src.dimensions().1 as i32) {
+                        if (x_dst as i32 >= 0)
+                            && ((x_dst as i32) < image.dimensions().0 as i32)
+                            && (y_dst as i32 >= 0)
+                            && ((y_dst as i32) < image.dimensions().1 as i32)
+                        {
                             // println!("{},{} -> {},{}", x_src as i32, y_src as i32, x, y);
-                            dest_img.put_pixel(x_dst as u32, y_dst as u32, hom_src.get_pixel(x as u32, y as u32).to_rgb());
+                            dest_img.put_pixel(
+                                x_dst as u32,
+                                y_dst as u32,
+                                image.get_pixel(x as u32, y as u32).to_rgb(),
+                            );
                         }
 
                         let (x_dst, y_dst) = apply_homography(&h, (x as f32, y as f32 + 0.5));
-                        if (x_dst as i32 >= 0) && ((x_dst as i32) < hom_src.dimensions().0 as i32)
-                        && (y_dst as i32 >= 0) && ((y_dst as i32) < hom_src.dimensions().1 as i32) {
+                        if (x_dst as i32 >= 0)
+                            && ((x_dst as i32) < image.dimensions().0 as i32)
+                            && (y_dst as i32 >= 0)
+                            && ((y_dst as i32) < image.dimensions().1 as i32)
+                        {
                             // println!("{},{} -> {},{}", x_src as i32, y_src as i32, x, y);
-                            dest_img.put_pixel(x_dst as u32, y_dst as u32, hom_src.get_pixel(x as u32, y as u32).to_rgb());
+                            dest_img.put_pixel(
+                                x_dst as u32,
+                                y_dst as u32,
+                                image.get_pixel(x as u32, y as u32).to_rgb(),
+                            );
                         }
 
                         let (x_dst, y_dst) = apply_homography(&h, (x as f32 + 0.5, y as f32 + 0.5));
-                        if (x_dst as i32 >= 0) && ((x_dst as i32) < hom_src.dimensions().0 as i32)
-                        && (y_dst as i32 >= 0) && ((y_dst as i32) < hom_src.dimensions().1 as i32) {
+                        if (x_dst as i32 >= 0)
+                            && ((x_dst as i32) < image.dimensions().0 as i32)
+                            && (y_dst as i32 >= 0)
+                            && ((y_dst as i32) < image.dimensions().1 as i32)
+                        {
                             // println!("{},{} -> {},{}", x_src as i32, y_src as i32, x, y);
-                            dest_img.put_pixel(x_dst as u32, y_dst as u32, hom_src.get_pixel(x as u32, y as u32).to_rgb());
+                            dest_img.put_pixel(
+                                x_dst as u32,
+                                y_dst as u32,
+                                image.get_pixel(x as u32, y as u32).to_rgb(),
+                            );
                         }
                     }
                 }
                 show_image(&DynamicImage::ImageRgb8(dest_img));
             }
-            _ => println!("err")
+            _ => println!("err"),
         }
     }
 }
